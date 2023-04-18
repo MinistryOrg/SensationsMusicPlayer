191818ac7790f54cc03e534e2d345d081b4072fd
root <root@192.168.2.6>
2023-04-18 17:49:10 +0300

    fix the album cover

--- app/src/main/java/com/mom/sensationsmusicplayer/repository/SongsRepoImpl.kt
+++ app/src/main/java/com/mom/sensationsmusicplayer/repository/SongsRepoImpl.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.net.Uri
 import android.provider.MediaStore
 import com.mom.sensationsmusicplayer.data.Song
-
 class SongsRepoImpl : SongsRepo{
     override fun getSongs(context: Context): List<Song> {
         val songsList = mutableListOf<Song>()
@@ -13,6 +12,7 @@ class SongsRepoImpl : SongsRepo{
 
         val projection = arrayOf(
             MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.ALBUM_ID,
             MediaStore.Audio.Media.TITLE,
             MediaStore.Audio.Media.ARTIST,
             MediaStore.Audio.Media.ALBUM,
@@ -24,6 +24,7 @@ class SongsRepoImpl : SongsRepo{
 
         context.contentResolver.query(uri, projection, null, null, sortOrder)?.use { cursor ->
             val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val albumIdColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID)
             val titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
             val artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
             val albumColumns = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)
@@ -31,11 +32,12 @@ class SongsRepoImpl : SongsRepo{
 
             while (cursor.moveToNext()) {
                 val id = cursor.getLong(idColumn)
+                val albumId = cursor.getLong(albumIdColumn)
                 val title = cursor.getString(titleColumn)
                 val artist = cursor.getString(artistColumn)
                 val duration = cursor.getLong(durationColumn)
                 val album = cursor.getString(albumColumns)
-                val albumCover = ContentUris.withAppendedId(albumArtUri, id)
+                val albumCover = ContentUris.withAppendedId(albumArtUri, albumId)
                 val songUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
 
                 val song = Song(id, title, artist,album,albumCover.toString(), duration, songUri)
